9c17882eea33b7c2da0a5b9a49816fc9e5c0777d
sergiokubo <sergio.t.kubo@gmail.com>
2024-10-14 17:49:49 -0300

    Projeto desafio criado

---
+++ src/main/java/com/sergiokubo/modelo_ORM/ModeloOrmApplication.java
@@ -0,0 +1,13 @@
+package com.sergiokubo.modelo_ORM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeloOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergiokubo/modelo_ORM/ModeloOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiokubo.modelo_ORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeloOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
